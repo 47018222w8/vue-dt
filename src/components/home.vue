de8b9036438de959b27463bdf731e80380e9c224
zwq <1623754384@qq.com>
2017-09-20 21:27:43 +0800

    second

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="c-grail">
-    <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" >
+    <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
       <el-menu-item index="1">处理中心</el-menu-item>
       <el-submenu index="2">
         <template slot="title">我的工作台</template>
@@ -13,30 +13,19 @@
       </el-menu-item>
     </el-menu>
     <div class="c-body">
-        <el-col :span="8" class="c-nav">
-          <el-menu default-active="2" class="c-menu" >
-            <el-submenu index="1">
-              <template slot="title">
-                <i class="el-icon-message"></i>导航一</template>
-              <el-menu-item-group>
-                <template slot="title">分组一</template>
-                <el-menu-item index="1-1">选项1</el-menu-item>
-                <el-menu-item index="1-2">选项2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="分组2">
-                <el-menu-item index="1-3">选项3</el-menu-item>
-              </el-menu-item-group>
-              <el-submenu index="1-4">
-                <template slot="title">选项4</template>
-                <el-menu-item index="1-4-1">选项1</el-menu-item>
-              </el-submenu>
-            </el-submenu>
-            <el-menu-item index="2">
-              <i class="el-icon-menu"></i>导航二</el-menu-item>
-            <el-menu-item index="3">
-              <i class="el-icon-setting"></i>导航三</el-menu-item>
-          </el-menu>
-        </el-col>
+      <el-col :span="8" class="c-nav">
+        <el-menu class="c-menu" theme="dark">
+          <el-submenu index="1">
+            <template slot="title">统计数据</template>
+              <el-menu-item index="1-1">医生术士统计列表</el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">基础数据</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-col>
 
     </div>
     <div class="c-footer">
